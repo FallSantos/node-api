5d5c519b50909015cab0f55a77aa579a385f1fb8
Flavio Santos <fall-_santos@hotmail.com>
2019-07-30 20:48:06 -0300

    Added procfile to deploy to heroku.

--- app/config/db.js
+++ app/config/db.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/nodeapi', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
 
 const db = mongoose.connection
     db.on('error', console.error.bind(console, 'connection error: '))
