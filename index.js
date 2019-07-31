b924ee01ff02e0df99c51feab61dadcfce762fa7
Flavio Santos <fall-_santos@hotmail.com>
2019-07-30 21:03:00 -0300

    Alter port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const app = require('./app/config/server')
 
-const port = process.env.PORT
+const port = process.env.PORT || 3005
 
 app.listen(port, () => {
     console.log(`>>> Server running on port ${port}...`)
