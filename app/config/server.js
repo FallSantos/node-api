ee34e28857af973a7b2840311c6e844b4fdf5382
Flavio Santos <fall-_santos@hotmail.com>
2020-02-08 20:37:20 -0300

    *

--- app/config/server.js
+++ app/config/server.js
@@ -14,7 +14,19 @@ require('./db')
 
 // Rotas
 app.get('/', (req, res) => {
-    res.send('api')
+    res.json({
+        "name": "Atarter Rocketseat",
+        "version": "0.1",
+        "author": [
+            {
+                "name": "Fall Santos",
+                "rule": "Fullstack developer",
+                "email": "fallsantosdev@hotmail.com",
+                "github": "https://github.com/fallsantos",
+                "linkedin": "https://www.linkedin.com/in/fall-santos-23bb87103/"
+            }
+        ]
+    })
 })
 
 require('../src/routes/product')(app)
