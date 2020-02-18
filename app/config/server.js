e1a72e1bc281de2d94e9b61a67ec3d3c1b244e74
fallsantos <fallsantosdev@hotmail.com>
2020-02-18 12:47:48 -0300

    Added morgan to log

--- app/config/server.js
+++ app/config/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
@@ -12,10 +12,24 @@ app.use(express.json())// Permitir receber dados no formato json.
 // Conexão
 require('./db')
 
+app.use(morgan('dev'))
+
 // Rotas
 app.get('/', (req, res) => {
-    res.send('api')
-})
+    return res.json({
+      "name": "Api para o Starter",
+      "version": "0.1",
+      "author": [
+        {
+          "name": "Fall Santos",
+          "rule": "Fullstack developer",
+          "email": "fallsantosdev@hotmail.com",
+          "github": "https://github.com/fallsantos",
+          "linkedin": "https://www.linkedin.com/in/fall-santos-23bb87103/"
+        }
+      ]
+    })
+  })
 
 require('../src/routes/product')(app)
 
